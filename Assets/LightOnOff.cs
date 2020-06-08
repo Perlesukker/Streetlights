93a1e34227302af584a2d4cc73d6726ae9b8f7d7
Charlotte Victoria Molnar <charlottevmolnar@gmail.com>
2020-06-08 23:00:45 +0200

    Upload entire project to Git

---
+++ Assets/LightOnOff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOnOff : MonoBehaviour
+{
+    public Light myLight;
+    public bool amIOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        amIOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myLight.enabled = amIOn;
+    }
+
+    public void OnOff()
+    {
+        amIOn = !amIOn;
+    }
+}
---
+++ Assets/Sun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sun : MonoBehaviour
+{
+    public Slider mySlide;
+
+    public Light daSun;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(mySlide.value, transform.rotation.y, transform.rotation.z);
+    }
+}
